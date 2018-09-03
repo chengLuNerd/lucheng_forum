46ac655412cac4115f7ed5306206f0b04c40fc57
cheng.lu <cheng_lu@me.com>
2018-09-04 07:28:35 +0800

    login html update

--- lucheng/auth/forms.py
+++ lucheng/auth/forms.py
@@ -1,18 +1,20 @@
 """
-    flaskbb.auth.forms
-    ~~~~~~~~~~~~~~~~~~
+flaskbb.auth.forms .
 
-    It provides the forms that are needed for the auth views.
+It provides the forms that are needed for the auth views.
 
-    :copyright: (c) 2014 by the FlaskBB Team.
-    :license: BSD, see LICENSE for more details.
+:copyright: (c) 2014 by the FlaskBB Team.
+:license: BSD, see LICENSE for more details.
 """
 
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, BooleanField, SubmitField)
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
+    """Login Form define."""
+
     login = StringField("Username or Email address", validators=[
         DataRequired(message="Please enter your username or email address.")
     ])
